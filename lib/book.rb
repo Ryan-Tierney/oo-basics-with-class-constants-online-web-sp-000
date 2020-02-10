d298dfef619c2b3b66759d2018cc697afb96fdb9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-02-10 17:52:21 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,6 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title :genre 
   
-  def genre=(genre)
-    @genre = genre 
-    GENRES << genre 
-  end 
-    
-  
   GENRES = []
 
   def initialize(title)
@@ -17,5 +11,6 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 
 end
